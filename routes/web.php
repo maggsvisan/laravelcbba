0ca97b763450f82cffb0e3acf7e54af68f3e9134
Margarita Villarreal <maggsvisan@Margaritas-MacBook-Pro.local>
2017-12-01 00:59:30 -0600

    test

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,27 @@
 */
 
 Route::get('/test2', function () {
-	return "raspberry";
-    return view('welcome');
+	$conn = mysqli_connect("mmydbpuli.clsxlmrwgdqn.us-east-2.rds.amazonaws.com", "puliroot", "puliroot2017", "CommunityPuli");
+    
+    if ($conn != null){
+        
+        $sql = "SELECT id, fName, lName, passwrd, accessType FROM Users WHERE matricula = '$mat'";
+    
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0)
+        {
+            $row = $result -> fetch_assoc();
+            $conn -> close();
+        
+            return array("idUser" => $row["matricula"], "fName" => $row["fName"], "lName" => $row["lName"],
+                         "password" => $row["passwrd"], "accessType" => $row["accessType"], "status" => "SUCCESS");
+        }
+        else{
+            $conn -> close();
+            return array("status" => "USERNAME NOT FOUND");
+        }
+    }else{
+        $conn -> close();
+        return array("status" => "CONNECTION WITH DB WENT WRONG");
+    }
 });
