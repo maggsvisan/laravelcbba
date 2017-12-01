6f6fc69bdb7aadb04f82e910f7512a4e350376b7
Margarita Villarreal <maggsvisan@Margaritas-MacBook-Pro.local>
2017-12-01 01:05:03 -0600

    db setting

--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use DB;
+use Response;
 
 Route::get('/test2', function () {
-	$conn = mysqli_connect("mmydbpuli.clsxlmrwgdqn.us-east-2.rds.amazonaws.com", "puliroot", "puliroot2017", "CommunityPuli");
-    
-    if ($conn != null){
-        
-        $sql = "SELECT id, fName, lName, passwrd, accessType FROM Users WHERE matricula = '$mat'";
-    
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0)
-        {
-            $row = $result -> fetch_assoc();
-            $conn -> close();
-        
-            return array("idUser" => $row["matricula"], "fName" => $row["fName"], "lName" => $row["lName"],
-                         "password" => $row["passwrd"], "accessType" => $row["accessType"], "status" => "SUCCESS");
-        }
-        else{
-            $conn -> close();
-            return array("status" => "USERNAME NOT FOUND");
-        }
-    }else{
-        $conn -> close();
-        return array("status" => "CONNECTION WITH DB WENT WRONG");
-    }
+	$mat = 'a0133455';
+	$rows = DB::raw('SELECT id, fName, lName, passwrd, accessType FROM Users WHERE matricula = "$mat"');
+
+	return Response::json($rows);
 });
